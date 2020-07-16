760d21cdcb3534de28890af6ea0253ee8a4c47f7
dmstjd1024 <dmstjd1024@naver.com>
2020-07-17 05:01:57 +0900

    intellij init

---
+++ src/test/java/com/dmstjd1024/toyproject/ToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmstjd1024.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
