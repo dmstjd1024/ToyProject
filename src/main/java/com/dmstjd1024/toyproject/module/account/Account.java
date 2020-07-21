ef13c0dceaa097ef046692baa8fccd17cb4c5e56
dmstjd1024 <dmstjd1024@naver.com>
2020-07-21 19:13:33 +0900

    refactoring 및 회원가입 후 redirect을 login으로 변경

--- src/main/java/com/dmstjd1024/toyproject/module/account/Account.java
+++ src/main/java/com/dmstjd1024/toyproject/module/account/Account.java
@@ -2,10 +2,7 @@ package com.dmstjd1024.toyproject.module.account;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor @NoArgsConstructor
--- src/main/java/com/dmstjd1024/toyproject/module/account/AccountController.java
+++ src/main/java/com/dmstjd1024/toyproject/module/account/AccountController.java
@@ -38,7 +38,7 @@ public class AccountController {
         }
 
         accountService.saveNewAccount(signUpForm);
-        return "redirect:/";
+        return "redirect:/login";
     }
 
 }
