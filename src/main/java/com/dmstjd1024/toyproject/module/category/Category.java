ae552414df1875a14acb89938c3886d39434f535
dmstjd1024 <dmstjd1024@naver.com>
2020-08-28 05:23:31 +0900

    category id 생성방식 변환

--- src/main/java/com/dmstjd1024/toyproject/module/category/Category.java
+++ src/main/java/com/dmstjd1024/toyproject/module/category/Category.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,7 +16,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class Category{
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
